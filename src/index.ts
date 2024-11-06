9650af4e0877171a5fc04eaeedffb6c05840a027
Combrero <fchel.sea.v@gmail.com>
2024-11-07 01:09:25 +0700

    first

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import connectDB from './db-connection';
+import bookrouter from './routes/book.route';
+
+const app = express();
+
+app.use(express.json());
+connectDB();
+
+app.get("/", (_, response) => {
+  response.status(200).send("Server is up and running 💫");
+});
+
+app.use("/books", bookrouter);
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Express is running on Port ${PORT}`);
+});
